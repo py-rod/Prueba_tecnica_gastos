b0e1117a232fd65027deec030117737ffc0b66d3
Pyrod <pyrod7@gmail.com>
2024-08-03 21:31:08 -0600

    Se creo los serializer

--- savings_targets/serializer.py
+++ savings_targets/serializer.py
@@ -1,1 +1,20 @@
 from rest_framework import serializers
+from .models import SavingsTargets
+
+
+class CreateSavingsTargetsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SavingsTargets
+        fields = ['quantity', 'description', 'init_date', 'last_date']
+
+
+class GetAllSavingsTargetsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SavingsTargets
+        fields = '__all__'
+
+
+class UpgradeSavingsTargetsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SavingsTargets
+        fields = ['quantity', 'description', 'init_date', 'last_date']
