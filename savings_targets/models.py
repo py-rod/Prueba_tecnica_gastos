16a7d7e628e58b3f05ed814e892605ad1c30968d
Pyrod <pyrod7@gmail.com>
2024-08-03 21:11:41 -0600

    se creo el modelo

--- savings_targets/models.py
+++ savings_targets/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from type_of_savings.models import TypeOfSavings
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+
+class SavingsTargets(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    quantity = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(max_length=400)
+    type_saving = models.ForeignKey(TypeOfSavings, on_delete=models.CASCADE)
+    init_date = models.DateField()
+    last_date = models.DateField()
+
+    def __str__(self) -> str:
+        return f'{self.user} - {self.quantity} - {self.type_saving}'
