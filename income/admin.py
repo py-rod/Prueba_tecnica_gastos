26ea92a7e9e7c6d9c7ff3961ccb0a4bcf82a011b
Pyrod <pyrod7@gmail.com>
2024-08-03 20:07:48 -0600

    Se agrego la vista para el admin

--- income/admin.py
+++ income/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Income
 # Register your models here.
+
+
+@admin.register(Income)
+class IncomeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'quantity', 'date']
+    list_display_links = ['id', 'user']
+    list_per_page = 50
+    search_fields = ['id', 'user']
