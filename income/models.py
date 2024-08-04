a2cff162b237cebeb2a61997c07a9145d0ebd478
Pyrod <pyrod7@gmail.com>
2024-08-03 20:05:30 -0600

    Se creo el modelo

--- income/models.py
+++ income/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+
+class Income(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    quantity = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(max_length=400)
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f'{self.user} - {self.quantity}'
+
+    class Meta:
+        verbose_name_plural = 'Incomes'
