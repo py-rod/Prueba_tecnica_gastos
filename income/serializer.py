4b7106e91aeff83dcd0cce99d57c54f026285762
Pyrod <pyrod7@gmail.com>
2024-08-03 20:24:53 -0600

    Se crearon los serializer

--- income/serializer.py
+++ income/serializer.py
@@ -1,1 +1,20 @@
 from rest_framework import serializers
+from .models import Income
+
+
+class CreateIncomeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Income
+        fields = ['quantity', 'description']
+
+
+class GetAllIncomeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Income
+        fields = '__all__'
+
+
+class UpgradeIncomeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Income
+        fields = ['quantity', 'description']
