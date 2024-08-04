0f3e316ce62d6ffbd2479a96b0add68f41106529
Pyrod <pyrod7@gmail.com>
2024-08-03 20:48:43 -0600

    Se agregaron las demas rutas

--- income/urls.py
+++ income/urls.py
@@ -4,5 +4,13 @@ from . import views
 
 urlpatterns = [
     path('create_income', views.create_income, name='create_income'),
+    path('get_all_income_by_user', views.get_all_income_by_user,
+         name='get_all_income_by_user'),
+    path('get_all_income_by_admin', views.get_all_income_by_admin,
+         name='get_all_income_by_admin'),
+    path('upgrade_income/<int:id_income>',
+         views.upgrade_income, name='upgrade_income'),
+    path('delete_income/<int:id_income>',
+         views.delete_income, name='delete_income')
 
 ]
